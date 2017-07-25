f3bd6fe654df081324d871782b83b8a017542fcc
immanoj16 <mmaharana14@gmail.com>
2017-07-26 01:00:45 +0530

    Add intent to send to mail

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,6 +9,8 @@ package com.example.android.justjava;
  */
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,14 +19,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.NumberFormat;
-
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +37,23 @@ public class MainActivity extends AppCompatActivity {
             // Show an error message as a toast
             Toast.makeText(this, "You can not have more than 100 coffees", Toast.LENGTH_SHORT).show();
             // Exit this method early because there is nothing left to do
+            return;
         }
-        quantity += 1;
-        display(quantity);
+
+        quantity++;
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
-        quantity -= 1;
-        display(quantity);
+        if (quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You can not have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there is nothing left to do
+            return;
+        }
+
+        quantity--;
+        displayQuantity(quantity);
     }
 
     /**
@@ -62,7 +71,14 @@ public class MainActivity extends AppCompatActivity {
 
         int price = calculatePrice(hasWhippedCream, hasChocolate);
         String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
-        displayMessage(priceMessage);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "JustJava order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
@@ -102,25 +118,9 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
     }
 
 }
